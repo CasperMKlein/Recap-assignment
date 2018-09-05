d4af627449910396f67e11734608fe53b4b617ea
Kristian Ramsnæs <4140kristian@gmail.com>
2018-09-05 15:08:36 +0200

    Controller class updated

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import Model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    List<Student> studentList = new ArrayList<>();
+    List<Course> courseList = new ArrayList<>();
+    List<Exam>  examList = new ArrayList<>();
+    List<Subject> subjectList = new ArrayList<>();
+    List<Teacher> teacherList = new ArrayList<>();
+
+    
+
 }
